a19ef1ae195d36297da32ab080803447798bc353
priteshkokane26 <145313228+priteshkokane26@users.noreply.github.com>
2024-01-22 21:03:31 +0530

    conversion operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,30 @@
+// conversion
+
+// let score =  Boolean //"26"
+
+// let value1 = Number(score);
+// console.log(typeof value1)
+// console.log(typeof(value1))
+
+// let num ="123abc"
+// let value2 = Number(num); 
+//using Number it will be converted to Number but in actual it is NaN 
+// console.log(typeof value2);
+// console.log( value2) //NaN 
+
+// "26" => 26
+// "26abc" =>NaN
+// true => 1; false => 0
+
+// let isLogin = "ram"
+
+// let state = Boolean(isLogin)
+// console.log(state)
+
+// true => 1; false => 0
+//"abc" => true; " " => false
+
+let str = "Jai Shree Ram"
+let stringConvert = String(str)
+console.log(typeof stringConvert);
+console.log(stringConvert);
